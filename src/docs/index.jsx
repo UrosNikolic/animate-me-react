4e26a49c324fdec122c6b51ccd0c0f6c906e2281
Uros Nikolic <unikolic94@gmail.com>
2018-07-15 21:03:44 +0200

    setting up minimalistic react animation library

--- src/docs/index.jsx
+++ src/docs/index.jsx
@@ -1,13 +1,20 @@
 import React from 'react'
 import { render } from 'react-dom'
-import MyComponent from '../../lib'
+import AnimateMe from '../../lib'
 import './styles.css'
 
 function Demo() {
   return (
     <div>
       <h1>Demo with examples of the component</h1>
-      <MyComponent color='brown'>Wow what a button</MyComponent>
+        <div style={{marginTop: '1500px'}}>
+            <AnimateMe
+                transitionDuration={3.5}
+                opacity={0.1}
+                transform={'translateX(750px)'}>
+                <h3>teeest</h3>
+            </AnimateMe>
+        </div>
     </div>
   )
 }
--- src/lib/index.jsx
+++ src/lib/index.jsx
@@ -1,18 +1,76 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
-class MyComponent extends Component {
-  handleClick() {
-    console.log('Test Click!')
-  }
+class AnimateMe extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            position: {},
+            animatingStyles: {}
+        };
 
-  render() {
-    const { color, children } = this.props
-    return (
-      <button onClick={this.handleClick} style={{ color }}>
-        {children}
-      </button>
-    )
-  }
+        this.myRef = React.createRef();
+        this.onScroll = this.onScroll.bind(this);
+        this.addAnimatingStyles = this.addAnimatingStyles.bind(this);
+    }
+
+    isAnimated = false;
+
+    componentDidMount() {
+        window.addEventListener('scroll', this.onScroll);
+    }
+
+    onScroll() {
+        this.setState({
+            position: this.myRef.current.getBoundingClientRect()
+        });
+
+        if ((this.state.position.top < window.innerHeight) && !this.isAnimated) {
+            this.addAnimatingStyles();
+            this.isAnimated = true;
+            window.removeEventListener('scroll', this.onScroll);
+        }
+    }
+
+    addAnimatingStyles() {
+        this.setState({
+            animatingStyles: {
+                transform: this.props.transform,
+                transformOrigin: this.props.transformOrigin,
+                transitionDuration: `${this.props.transitionDuration}s`,
+                transitionTimingFunction: this.props.transitionTimingFunction,
+                transitionDelay: this.props.transitionDelay,
+                opacity: this.props.opacity,
+                marginTop: `${this.props.marginTop}px`,
+                marginRight: `${this.props.marginRight}px`,
+                marginBottom: `${this.props.marginBottom}px`,
+                marginLeft: `${this.props.marginLeft}px`,
+                width: this.props.width,
+                height: this.props.height,
+            }
+        })
+    }
+
+    render() {
+        return (
+            <div className='animation-wrapper' style={this.state.animatingStyles} ref={this.myRef}>
+                {this.props.children}
+            </div>
+        )
+    }
 }
 
-export default MyComponent
+AnimateMe.propTypes = {
+    transform: PropTypes.string,
+    transformOrigin: PropTypes.string,
+    transitionDuration: PropTypes.number,
+    transitionTimingFunction: PropTypes.string,
+    transitionDelay: PropTypes.string,
+    opacity: PropTypes.number,
+    marginRight: PropTypes.string,
+    marginLeft: PropTypes.string,
+    width: PropTypes.string,
+    height: PropTypes.string,
+};
+
+export default AnimateMe;
